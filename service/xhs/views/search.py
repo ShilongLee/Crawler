feccd3646df56a36c1c023c907d924ffcc2fb496
付伟 <foll.fu@hrtps.com>
2024-07-31 15:10:05 +0800

    增加搜索方式

--- service/xhs/views/search.py
+++ service/xhs/views/search.py
@@ -5,9 +5,10 @@ from lib.logger import logger
 from ..logic import request_search
 import random
 
-async def search(keyword: str, offset: int = 0, limit: int = 20):
+async def search(keyword: str, sort: str, offset: int = 0, limit: int = 20):
     """
     获取笔记搜索
+    "sort": "time_descending",  # 排序，general：默认, popularity_descending：最热, time_descending：最新
     """
     _accounts = await accounts.load()
     random.shuffle(_accounts)
@@ -15,8 +16,8 @@ async def search(keyword: str, offset: int = 0, limit: int = 20):
         if account.get('expired', 0) == 1:
             continue
         account_id = account.get('id', '')
-        res = await request_search(keyword, account.get('cookie', ''), offset, limit)
-        logger.info(f'search success, account: {account_id}, keyword: {keyword}, offset: {offset}, limit: {limit}, res: {res}')
+        res = await request_search(keyword, account.get('cookie', ''), sort, offset, limit)
+        logger.info(f'search success, account: {account_id}, keyword: {keyword}, sort: {sort}, offset: {offset}, limit: {limit}, res: {res}')
         return reply(ErrorCode.OK, '成功' , res)
-    logger.warning(f'search failed. keyword: {keyword}, offset: {offset}, limit: {limit}')
+    logger.warning(f'search failed. keyword: {keyword}, sort: {sort}, offset: {offset}, limit: {limit}')
     return reply(ErrorCode.NO_ACCOUNT, '请先添加账号')
