566584c64c251e5f634133b7e4e01b65eb701ead
付伟 <foll.fu@hrtps.com>
2024-07-31 15:09:37 +0800

    增加搜索方式

--- service/xhs/logic/search.py
+++ service/xhs/logic/search.py
@@ -2,7 +2,7 @@ from .common import common_request
 import execjs
 import asyncio
 
-async def request_search(keyword: str, cookie: str, offset: int = 0, limit: int = 20) -> dict:
+async def request_search(keyword: str, cookie: str, sort: str, offset: int = 0, limit: int = 20) -> dict:
     """
     请求小红书获取搜索信息
     """
@@ -10,7 +10,7 @@ async def request_search(keyword: str, cookie: str, offset: int = 0, limit: int 
     start_page = int( offset / page_size ) + 1
     end_page = int((offset + limit - 1) / page_size) + 1
     xhs_sign_obj = execjs.compile(open('lib/js/xhs.js', encoding='utf-8').read())
-    tasks = [request_page(page, keyword, cookie, page_size, xhs_sign_obj) for page in range(start_page, end_page + 1)]
+    tasks = [request_page(page, keyword, cookie, sort, page_size, xhs_sign_obj) for page in range(start_page, end_page + 1)]
     pages = await asyncio.gather(*tasks)
     results = []
     for result in pages:
@@ -18,14 +18,14 @@ async def request_search(keyword: str, cookie: str, offset: int = 0, limit: int 
     results = results[(offset % page_size):(offset % page_size + limit)]
     return results
 
-async def request_page(page: int, keyword: str, cookie: str, page_size: int, xhs_sign_obj) -> list:
+async def request_page(page: int, keyword: str, cookie: str, sort: str, page_size: int, xhs_sign_obj) -> list:
     headers = {"cookie": cookie}
     params = {
         "ext_flags": [],
         "image_formats": ["jpg", "webp", "avif"],
         "keyword": keyword,
         "note_type": 0,
-        "sort": "general",
+        "sort": sort,
         "page": page,
         "page_size": page_size,
         'search_id': xhs_sign_obj.call('searchId')
